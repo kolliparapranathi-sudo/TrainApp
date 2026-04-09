5afbe6a2f6a07d2700e1656a2fe35affced00e2d
Pranathi Kollipara <kolliparapranathi@gmail.com>
2026-04-09 10:53:21 +0530

    Pranathi Added UC3-Unique Bogie IDs using HashSet

--- src/main/TrainApp.java
+++ src/main/TrainApp.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class TrainApp {
 
@@ -9,31 +11,43 @@ public class TrainApp {
         System.out.println(" Train Consist Management App");
         System.out.println("======================================");
 
-        // Create ArrayList for passenger bogies
+        // ================= UC1 =================
+        List<String> trainConsist = new ArrayList<>();
+        System.out.println("Train consist initialized.");
+        System.out.println("Initial bogie count: " + trainConsist.size());
+
+        // ================= UC2 =================
         List<String> passengerBogies = new ArrayList<>();
 
-        // Add bogies
         passengerBogies.add("Sleeper");
         passengerBogies.add("AC Chair");
         passengerBogies.add("First Class");
 
-        // Display bogies
         System.out.println("\nPassenger Bogies after addition:");
         System.out.println(passengerBogies);
 
-        // Remove one bogie
         passengerBogies.remove("AC Chair");
 
         System.out.println("\nAfter removing AC Chair:");
         System.out.println(passengerBogies);
 
-        // Check existence
         boolean exists = passengerBogies.contains("Sleeper");
         System.out.println("\nIs Sleeper present? " + exists);
 
         System.out.println("\nFinal Bogie List:");
         System.out.println(passengerBogies);
 
-        System.out.println("\nProgram continues...");
+        // ================= UC3 =================
+        Set<String> bogieIDs = new HashSet<>();
+
+        bogieIDs.add("BG101");
+        bogieIDs.add("BG102");
+        bogieIDs.add("BG103");
+        bogieIDs.add("BG101"); // duplicate
+
+        System.out.println("\nUnique Bogie IDs:");
+        System.out.println(bogieIDs);
+
+        System.out.println("\nProgram running...");
     }
 }
